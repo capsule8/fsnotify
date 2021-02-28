406795c7e9980283ca51d0de5e03c0b85414cec0
Ryan Petrich <ryan@capsule8.com>
2021-02-28 18:13:04 -0500

    Don't ignore file modification events for files that are deleted since it may be relevant to receive notification of a file write that happened right before a delete

--- inotify.go
+++ inotify.go
@@ -10,7 +10,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"os"
 	"path/filepath"
 	"strings"
 	"sync"
@@ -295,23 +294,13 @@ func (w *Watcher) readEvents() {
 }
 
 // Certain types of events can be "ignored" and not sent over the Events
-// channel. Such as events marked ignore by the kernel, or MODIFY events
-// against files that do not exist.
+// channel. Such as events marked ignore by the kernel.
 func (e *Event) ignoreLinux(mask uint32) bool {
 	// Ignore anything the inotify API says to ignore
 	if mask&unix.IN_IGNORED == unix.IN_IGNORED {
 		return true
 	}
 
-	// If the event is not a DELETE or RENAME, the file must exist.
-	// Otherwise the event is ignored.
-	// *Note*: this was put in place because it was seen that a MODIFY
-	// event was sent after the DELETE. This ignores that MODIFY and
-	// assumes a DELETE will come or has come if the file doesn't exist.
-	if !(e.Op&Remove == Remove || e.Op&Rename == Rename) {
-		_, statErr := os.Lstat(e.Name)
-		return os.IsNotExist(statErr)
-	}
 	return false
 }
 
